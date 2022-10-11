9cfeab3f5cd1da6410fef983e5088ccd4b4716a3
Dengel <52581244+DengelLionel@users.noreply.github.com>
2022-10-11 13:23:07 -0500

    Changed of lists.ts

--- src/data/lists.ts
+++ src/data/lists.ts
@@ -1,36 +1,36 @@
 export const listsGeneral=[
       
             {
-        "id":1,
-        "title":{"title":"Todo","color":"guiontodo"},
-        "totalCards":null,
-        "cards":[
+        id:1,
+        title:{title:"Todo",color:"guiontodo"},
+        totalCards:null,
+        cards:[
             {
-            "id":1,
-            "title":"Item1 Card",
-            "substacks":"0 of 3 substacks"
+            id:1,
+            title:"Item1 Card",
+            substacks:"0 of 3 substacks"
         },
         {
-            "id":2,
-            "title":"Item2 Card",
-            "substacks":"0 of 3 substacks"
+            id:2,
+            title:"Item2 Card",
+            substacks:"0 of 3 substacks"
         }
     ]
     },
     {
-        "id":2,
-        "title":{"title":"Doing","color":"guiondoing"},
-        "totalCards":null,
-        "cards":[
+        id:2,
+        title:{"title":"Doing","color":"guiondoing"},
+        totalCards:null,
+        cards:[
             {
-            "id":1,
-            "title":"Item1 Card",
-            "substacks":"0 of 2 substacks"
+            id:1,
+            title:"Item1 Card",
+            substacks:"0 of 2 substacks"
              },
              {
-                "id":2,
-                "title":"Item1 Card",
-                "substacks":"0 of 3 substacks"
+                id:2,
+                title:"Item1 Card",
+                substacks:"0 of 3 substacks"
                  }
         ]
     
